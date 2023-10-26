e5ffdd4a67d90263ddd364c5850b499179c27b6a
zhouziwei <2361319763@qq.com>
2023-10-26 16:13:34 +0800

    ✨ feat(tabs页面切换):添加批量删除和刷新功能

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,6 +1,9 @@
 import React, { ReactNode, useEffect, useState } from 'react';
 import KeepAlive,{ AliveScope, useAliveController } from 'react-activation';
 import { history } from '@umijs/max';
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
 import { PageContainer } from '@ant-design/pro-components';
 import { getMatchMenuItem } from '@/services/session';
 
@@ -10,21 +13,47 @@ interface PropsInterface {
 interface TabInterface {
   key: string;
   tab: string | undefined;
+  meta: {
+    noCache?: boolean;
+  }
 }
+
+const items: MenuProps['items'] = [{
+  label: (
+    <Space> 刷新页面 </Space>
+  ),
+  key: '0',
+},{
+  label: (
+    <Space> 关闭当前 </Space>
+  ),
+  key: '1',
+},{
+  label: (
+    <Space> 关闭其他 </Space>
+  ),
+  key: '2',
+},{
+  label: (
+    <Space> 全部关闭 </Space>
+  ),
+  key: '3',
+}]
 
 const MainContent: React.FC<PropsInterface> = (props) => {
   const { location } = history;
-  const { drop } = useAliveController();
+  const { drop, refresh } = useAliveController();
   const matchingMenuItem = getMatchMenuItem(location.pathname)[0] || null;
   const [ noCache, setNoCache ] = useState(false);
   const [ tabList, setTabList ] = useState<TabInterface[]>([]);
 
   useEffect(()=>{
-    setNoCache(matchingMenuItem?.meta?.noCache);
+    setNoCache(matchingMenuItem?.meta?.noCache || true);
     if (matchingMenuItem && tabList.findIndex(J=>J.key===location.pathname) === -1 && location.pathname!='/' && location.pathname!='/home' && matchingMenuItem?.component!='Layout') {
       setTabList([...tabList,{
         key: location.pathname,
-        tab: matchingMenuItem?.name
+        tab: matchingMenuItem?.name,
+        meta: matchingMenuItem?.meta
       }])
     }
     // console.log('MainContent history', matchingMenuItem, location, history);
@@ -45,11 +74,57 @@ const MainContent: React.FC<PropsInterface> = (props) => {
     setTabList(newTabList);
   }
 
+  const refreshSelectedTag = (key:string) => {
+    if (noCache) {
+      history.go(0);
+    } else {
+      refresh(key);
+    }
+  }
+
+  const closeOthersTags = () => {
+    tabList.forEach(J=>{
+      if(J.key!==location.pathname) {
+        !J.meta.noCache && drop(J.key)
+      }
+    })
+    setTabList(tabList.filter(J=>J.key===location.pathname));
+  }
+
+  const closeAllTags = () => {
+    tabList.forEach(J=>{
+      !J.meta.noCache && drop(J.key)
+    })
+    setTabList([]);
+    history.push('/');
+  }
+
+  const handleMenu = (key:string) => {
+    switch (key) {
+      case '0':
+        refreshSelectedTag(location.pathname);
+        break;
+      case '1':
+        handleTabRemove(location.pathname);
+        break;
+      case '2':
+        closeOthersTags();
+        break;
+      case '3':
+        closeAllTags();
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   return (
     <PageContainer
       tabList={tabList}
       tabProps={{
         type: 'editable-card',
+        size: 'small',
         hideAdd: true,
         onEdit: (e, action) => {
           action=='remove' && handleTabRemove(e);
@@ -57,6 +132,21 @@ const MainContent: React.FC<PropsInterface> = (props) => {
         onTabClick: (key, e) => history.push(key),
       }}
       tabActiveKey={location.pathname}
+      tabBarExtraContent={
+        <Dropdown menu={{
+          items,
+          onClick: ({ key })=>{
+            handleMenu(key);
+          }
+        }}>
+          <a onClick={e=>e.preventDefault()}>
+            <Space>
+              更多
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+      }
     >
       <AliveScope>
         {
